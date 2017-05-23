deb964322d6a92294dc66cf4bb543506a9f7f277
Mike <mgchapman24@gmail.com>
2017-05-23 11:50:16 -0400

    up to 8.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,15 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	#this allows us to fund user on everypage
+	# we use find_by instread of .find bc .find raises an expcetion if no suer instead of nil
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	#method to return true if user is logged in
+	def logged_in?
+		!current_user.nil?
+	end
+
 end
