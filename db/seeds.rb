3138cf92fda943dba8399be6ce1c07b223d82a3b
Mike <mgchapman24@gmail.com>
2017-05-25 10:37:23 -0400

    up to 10.3.3, about to do bundle install

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Example person",
+			 email: "example@exampleperson.com",
+			 password: "foobar",
+			 password_confirmation: "foobar")
+
+99.times do |n|
+	name = Faker::Name.name
+	email = "example-#{n+1}@fake.com"
+	password = "password"
+	User.create!(name: name,
+				 email: email,
+				 password: password,
+				 password_confirmation: password)
+end
