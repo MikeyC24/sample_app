538deaffaa0a6248ac2483d9b6470dee5f74be54
Mike <mgchapman24@gmail.com>
2017-05-25 15:40:58 -0400

    finished chap 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "Example person",
+User.create!(name: "example person",
 			 email: "example@exampleperson.com",
 			 password: "foobar",
-			 password_confirmation: "foobar")
+			 password_confirmation: "foobar",
+			 admin: true)
 
 99.times do |n|
 	name = Faker::Name.name
