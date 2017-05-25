2ae6d35533726b34f14b89dca45b31ba3f5b96c5
MikeyC24 <mgchapman24@gmail.com>
2017-05-25 07:57:26 -0400

    bundle issue, 10.2.4

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,6 +4,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+	def setup
+		@user = users(:michael)
+		@other_user = users(:archer)
+	end
 
 test "layout links" do
 	get root_path
@@ -12,6 +16,20 @@ test "layout links" do
 	assert_select "a[href=?]", help_path
 	assert_select "a[href=?]", about_path
 	assert_select "a[href=?]", contact_path
+	assert_select "a[href=?]", signup_path
+	assert_select "a[href=?]", login_path
+	log_in_as(@user)
+	get root_path
+	assert_select "a[href=?]", root_path, count: 2
+	assert_select "a[href=?]", help_path
+	assert_select "a[href=?]", about_path
+	assert_select "a[href=?]", contact_path
+	assert_select "a[href=?]", users_path
+	assert_select "a[href=?]", login_path, count: 0
+	assert_select "a[href=?]", logout_path
+	assert_select "a[href=?]", "#", text: "Account"
+	assert_select "a[href=?]", user_path(@user)
+	assert_select "a[href=?]", edit_user_path(@user)
 end
 
 
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,10 +18,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  test "successful edit" do
-  	log_in_as(@user) 
-  	get edit_user_path(@user)
-  	assert_template 'users/edit'
+  test "successful edit with friendlt forwarding" do
+  	get edit_user_path(@user) 
+  	log_in_as(@user)
+  	assert_redirected_to edit_user_url(@user)
   	name = "Foo Bar"
   	email = "foo@bar.com"
   	patch user_path(@user), params: { user: { name: name,
@@ -29,7 +29,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   											  password: "",
   											  password_confirmation: "" } }
   	assert_not flash.empty?
-  	assert_redirected_to @user
+  	assert_redirected_to session[:forwarding_url], nil
   	@user.reload
   	assert_equal name, @user.name
   	assert_equal email, @user.email
