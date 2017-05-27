1ee08f925ae966579dcc7eca8b633a4840284a49
MikeyC24 <mgchapman24@gmail.com>
2017-05-26 20:10:44 -0400

    fixed smtp error done with chap 11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://immense-plateau-23815.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smpt_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
