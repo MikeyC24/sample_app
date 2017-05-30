47315e878abec83b8eee4013cb6dcd9847c62234
MikeyC24 <mgchapman24@gmail.com>
2017-05-29 20:14:38 -0400

    up to 13.4

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -16,8 +16,19 @@ class MicropostsController < ApplicationController
 
 	def destroy
 		@micropost.destroy
-		flash[:success] "Micropost deleted" 
+		flash[:success] = "Micropost deleted" 
 		redirect_to request.referrer || root_url
+	end
+
+	def correct_user
+		@micropost = current_user.microposts.find_by(id: params[:id])
+		redirect_to root_url if @micropost.nil?
+	end
+
+	private
+
+	def micropost_params
+		params.require(:micropost).permit(:content)
 	end
 
 
