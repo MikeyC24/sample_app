63faeb62e527c932e36e141cf5d7e3a37e6ac81e
MikeyC24 <mgchapman24@gmail.com>
2017-05-31 07:55:40 -0400

    12.2.3, working on tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,6 +37,6 @@ end
 users = User.all
 user = users.first
 following = users[2..50]
-followers = users[3..20]
+followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
